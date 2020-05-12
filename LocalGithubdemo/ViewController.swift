846d4ca58111cb6fa625bedf27c3df0507b2893e
Nikhil Wagh <NW00594382@techmahindra.com>
2020-05-12 12:39:42 +0530

    Initial Commit

---
+++ LocalGithubdemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocalGithubdemo
+//
+//  Created by Nikhil Wagh on 12/05/20.
+//  Copyright © 2020 Nikhil Wagh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
